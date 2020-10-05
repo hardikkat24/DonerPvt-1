c7702d03590729ce77f86161037203630b5840f6
Saahil Lalwani <65405639+saahillalwani1@users.noreply.github.com>
2020-10-05 19:04:08 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -143,11 +143,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = 'login'
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.donerdiamonds.com.netsolmail.net'
+EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_HOST = os.environ.get('EMAIL_HOST')
 
-EMAIL_HOST_USER = 'kevin@donerdimaonds.com'
-EMAIL_HOST_PASSWORD = 'Kinaliada@1979'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 
 # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
